c239f9ebc0701e7ce2595f76cec6dbe83090a5a3
Yash Dhanwantri <yash.dhanwantri@nagarro.com>
2024-06-24 08:32:37 +0530

    Add project files.

---
+++ Singleton/Singleton.cs
@@ -0,0 +1,26 @@
+﻿namespace DesignPatternExample.Singleton
+{
+    // Sealed to make it non-inheritable.
+    public sealed class Singleton
+    {
+        // Private static property holding the instance of class.
+        private static Singleton _instance;
+
+        // Private constructor to avoid making instance of the class outside of it.
+        private Singleton()
+        {
+
+        }
+
+        // Static accessor to get the instance.
+        public static Singleton GetInstance()
+        {
+            if(_instance == null)
+            {
+                _instance = new Singleton();
+            }
+
+            return _instance;
+        }
+    }
+}
